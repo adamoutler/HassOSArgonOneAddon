3dd17a8bca2f85c325b8059a7d2565c59c03acb3
Kimmo Saari <kirbownz@me.com>
2021-07-30 15:43:47 +0300

    Minor refactoring and bug fix

--- argonOne/run.sh
+++ argonOne/run.sh
@@ -33,51 +33,51 @@ fcomp() {
 
 
 fanSpeedReport(){
-   percent=${1};
-   level=${2};
-   mode=${3};
-   temp=${4};
-   CorF=${5};
-   case ${level} in
-      1)
-        icon=mdi:fan;
-        ;;
-      2)
-        icon=mdi:fan-speed-1;
-        ;;
-      3)
-        icon=mdi:fan-speed-2;
-        ;;
-      4)
-        icon=mdi:fan-speed-3;
-        ;;
-      *)
-        icon=mdi:fan;
-    esac
-    reqBody='{"state": "'"${percent}"'", "attributes": { "unit_of_measurement": "%", "icon": "'"${icon}"'", "mode": "'"${mode}"'", "Temperature '"${CorF}"'": "'"${temp}"'", "fan level": "'"${level}"'", "friendly_name": "Argon Fan Speed"}}'
-    nc -i 1 hassio 80 1>/dev/null <<<unix2dos<<EOF
+  fanPercent=${1}
+  fanLevel=${2}
+  fanMode=${3}
+  cpuTemp=${4}
+  CorF=${5}
+  case ${fanLevel} in
+    1)
+      icon=mdi:fan;
+      ;;
+    2)
+      icon=mdi:fan-speed-1;
+      ;;
+    3)
+      icon=mdi:fan-speed-2;
+      ;;
+    4)
+      icon=mdi:fan-speed-3;
+      ;;
+    *)
+      icon=mdi:fan;
+  esac
+  reqBody='{"state": "'"${fanPercent}"'", "attributes": { "unit_of_measurement": "%", "icon": "'"${icon}"'", "mode": "'"${fanMode}"'", "Temperature '"${CorF}"'": "'"${cpuTemp}"'", "fan level": "'"${fanLevel}"'", "friendly_name": "Argon Fan Speed"}}'
+  nc -i 1 hassio 80 1>/dev/null <<< unix2dos<<EOF
 POST /homeassistant/api/states/sensor.argon_one_addon_fan_speed HTTP/1.1
 Authorization: Bearer ${SUPERVISOR_TOKEN}
 Content-Length: $( echo -ne "${reqBody}" | wc -c )
 
 ${reqBody}
 EOF
-};
+}
 
 
 
 action() {
-  percent=${1}
-  level=${2}
-  name=${3}
-  percentHex=${4}
-  temp=${5}
-  CorF=${6}
+  fanPercent=${1}
+  fanLevel=${2}
+  fanMode=${3}
+  cpuTemp=${4}
+  CorF=${5}
+  fanPercentHex=$(printf '%x' "${fanPercent}")
   printf '%(%Y-%m-%d_%H:%M:%S)T'
-  echo ": ${temp}${CorF} - Level ${level} - Fan ${percent}% (${name})";
-  i2cset -y 1 0x01a "${percentHex}"
+  echo ": ${cpuTemp}${CorF} - Level ${fanLevel} - Fan ${fanPercent}% (${fanMode})";
+  i2cset -y 1 0x01a "${fanPercentHex}"
   returnValue=${?}
-  test "${createEntity}" == "true" && fanSpeedReport "${percent}" "${level}" "${name}" "${temp}" "${CorF}" &
+  test "${createEntity}" == "true" && fanSpeedReport "${fanPercent}" "${fanLevel}" "${fanMode}" "${cpuTemp}" "${CorF}" &
   return ${returnValue}
 }
 
@@ -85,9 +85,9 @@ action() {
 #Inputs - inputs from Home Assistant Config
 ###
 CorF=$(jq -r '.CorF'<options.json)
-t1=$(mkfloat $(jq -r '.LowRange' <options.json))
-t2=$(mkfloat $(jq -r '.MediumRange'<options.json))
-t3=$(mkfloat $(jq -r '.HighRange'<options.json))
+t1=$(mkfloat "$(jq -r '.LowRange' <options.json)")
+t2=$(mkfloat "$(jq -r '.MediumRange'<options.json)")
+t3=$(mkfloat "$(jq -r '.HighRange'<options.json)")
 quiet=$(jq -r '.QuietProfile'<options.json)
 createEntity=$(jq -r '."Create a Fan Speed entity in Home Assistant"' <options.json)
 logTemp=$(jq -r '."Log current temperature every 30 seconds"' <options.json)
@@ -95,11 +95,11 @@ logTemp=$(jq -r '."Log current temperature every 30 seconds"' <options.json)
 ###
 #initial setup - prepare things for operation
 ###
-curPosition=-1;
-lastPosition=-1;
+fanLevel=-1;
+previousFanLevel=-1;
 
 #Trap exits and set fan to 100% like a safe mode.
-trap 'echo "Failed ${LINENO}: $BASH_COMMAND";i2cset -y 1 0x01a 0x63;lastPosition=-1;curPosition=-1; echo Safe Mode Activated!;' ERR EXIT INT TERM
+trap 'echo "Failed ${LINENO}: $BASH_COMMAND";i2cset -y 1 0x01a 0x63;previousFanLevel=-1;fanLevel=-1; echo Safe Mode Activated!;' ERR EXIT INT TERM
 
 if [ ! -e /dev/i2c-1 ]; then
   echo "Cannot find I2C port.  You must enable I2C for this add-on to operate properly";
@@ -119,9 +119,9 @@ fi;
 #Counts the number of repetitions so we can set a 10minute count. 
 thirtySecondsCount=0;
 #the current position, 0=unitialized. 1=off, 2=low, 3=medium, 4=high.
-curPosition=0;
+fanLevel=0;
 #The name of the current position.
-curPositionName="off";
+fanMode="off";
 #The human readable percentage of the fan speed
 fanPercent=0;
 
@@ -130,68 +130,68 @@ fanPercent=0;
 ###
 
 until false; do
-  read -r cpuRawTemp</sys/class/thermal/thermal_zone0/temp #read instead of cat fpr process reduction
+  read -r cpuRawTemp < /sys/class/thermal/thermal_zone0/temp #read instead of cat fpr process reduction
   cpuTemp=$(( cpuRawTemp/1000 )) #built-in bash math
-    unit="C"
+  unit="C"
+
   if [ "$CorF" == "F" ]; then #convert to F
     cpuTemp=$(( ( cpuTemp *  9/5 ) + 32 ));
     unit="F"
   fi
+  
   value=$(mkfloat $cpuTemp) #CPU Temp in floating point format.
   test "${logTemp}" == "true" && echo "Current Temperature $cpuTemp °$unit"
 
   #Choose a fan setting position by temperature comparison
   if ( fcomp "$value" '<=' "$t1" ); then
-    curPosition=1; #less than lowest
+    fanLevel=1; #less than lowest
   elif ( fcomp "$t1" '<=' "$value" && fcomp "$value" '<=' "$t2" ); then
-    curPosition=2; #between 1 and 2
+    fanLevel=2; #between 1 and 2
   elif ( fcomp "$t2" '<=' "$value" && fcomp "$value" '<=' "$t3" ); then
-    curPosition=3; #between 2 and 3
+    fanLevel=3; #between 2 and 3
   else
-    curPosition=4;
+    fanLevel=4;
   fi
+
   set +e
-  if [ $lastPosition != $curPosition ]; then
+
+  if [ $previousFanLevel != $fanLevel ]; then
     #based on current position, and quiet mode, we can set the name and percentage of fanspeed.
-    case $curPosition in
+    case $fanLevel in
       1)
-          curPosition=1;
-          curPositionName="OFF";
+          fanMode="OFF";
           fanPercent=0;
         ;;
       2)
-        curPosition=2;
         if [ "$quiet" != true ]; then
-          curPositionName="Low";
+          fanMode="Low";
           fanPercent=33;
         else
-          curPositionName="Quiet Low";
+          fanMode="Quiet Low";
           fanPercent=1;
         fi
         ;;
       3)
-        curPosition=3;
         if [ "$quiet" != true ]; then
-          curPositionName="Medium";
+          fanMode="Medium";
           fanPercent=66;
         else
-          curPositionName="Quiet Medium";
+          fanMode="Quiet Medium";
           fanPercent=3;
         fi
         ;;
-      *)
-        curPosition=4;
-        curPositionName="High";
+      4)
+        fanMode="High";
         fanPercent=100;
         ;;
     esac
-    fanPercentHex=$(printf '%x' ${fanPercent})
-    action "${curPosition}" "${fanPercent}" "${curPositionName}" "${fanPercentHex}" "${cpuTemp}" "${CorF}"
-    test $? -ne 0 && curPosition=lastPosition;
-    lastPosition=$curPosition;
+    action "${fanPercent}" "${fanLevel}" "${fanMode}" "${cpuTemp}" "${CorF}"
+    test $? -ne 0 && fanLevel=previousFanLevel
+    previousFanLevel=$fanLevel
   fi
-  sleep 30;
+  
+  sleep 30
   thirtySecondsCount=$((thirtySecondsCount + 1))
   #thirtySecondsCount mod 20 will be 0 once every 20 times, or approx. 10 minutes.
-  test $((thirtySecondsCount%20)) == 0 && test "${createEntity}" == "true" && fanSpeedReport "${percent}" "${level}" "${name}" "${cpuTemp}" "${CorF}"
+  test $((thirtySecondsCount%20)) == 0 && test "${createEntity}" == "true" && fanSpeedReport "${fanPercent}" "${fanLevel}" "${fanMode}" "${cpuTemp}" "${CorF}"
 done
